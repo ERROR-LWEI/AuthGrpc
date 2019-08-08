82ad7a9a56b6e77eaf8c816adb2b160494aa0f32
wei.liu <error_lwei@163.com>
2019-08-09 07:29:32 +0800

    2019-8-9

---
+++ src/lib/role/role.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, Injectable, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(
+        private readonly reflector: Reflector
+    ){}
+
+    canActivate(context: ExecutionContext): boolean {
+        const roles = this.reflector.get<string[]>('roles', context.getHandler());
+        if (!roles) {
+            return true;
+        }
+
+        
+    }
+}
