164eb7f1bf64ebac1874d8271b2de9adc38a981d
wei.liu <error_lwei@163.com>
2019-08-11 16:34:46 +0800

    2019-8-8 | pm2添加

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,29 @@ import AuthGrpcConnect from './grpc/authGrpc';
 import { Logger } from '@nestjs/common';
 import AllExceptions from './middleware/exceptions';
 import ResIntercept from './middleware/res.intercept';
+import { join } from 'path';
+import { existsSync, mkdirSync } from 'fs';
+
+
+async function initPm2() {
+  const pm2file = join(__dirname, '../pm2');
+  const pm2Error = join(pm2file, './error');
+  const pm2Out = join(pm2file, './out');
+  if (!existsSync(pm2file)) {
+    await mkdirSync(pm2file);
+  }
+  if (!existsSync(pm2Error)) {
+    await mkdirSync(pm2Error);
+  }
+  if(!existsSync(pm2Out)) {
+    await mkdirSync(pm2Out);
+  }
+}
+
+
 
 async function bootstrap() {
+  await initPm2();
   const app = await NestFactory.createMicroservice(AppModule, AuthGrpcConnect);
   app.useGlobalFilters(new AllExceptions())
   app.useGlobalInterceptors(new ResIntercept<any>());
