56f3d509fee54605385ea7610f3033227fdea019
wei.liu <error_lwei@163.com>
2019-08-11 21:28:49 +0800

    2019-8-11

--- src/grpc/authGrpc.ts
+++ src/grpc/authGrpc.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 const AuthGrpcConnect: ClientOptions = {
     transport: Transport.GRPC,
     options: {
-        url: 'localhost:6661',
+        url: '0.0.0.0:6661',
         package: 'auth',
         protoPath: join(__dirname, '../../proto/auth.proto'),
     }
